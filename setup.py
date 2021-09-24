1a68e15f848e37a14c95fd27ce72762559cb5182
Pavel Břoušek <brousek@ics.muni.cz>
2021-09-24 09:19:07 +0200

    fix: release to pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-arcgis-address",
-    version="1.0.0",
     url="https://github.com/melanger/django-arcgis-address.git",
     author="melanger",
-    author_email="brousek@ics.muni.cz",
+    author_email="pavelbrousek@melanger.cz",
     description="Django models for storing and retrieving postal addresses.",
     packages=find_packages(),
     install_requires=["setuptools"],
